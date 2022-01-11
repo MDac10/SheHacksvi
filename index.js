362be3fe258f0f0c12fb8a246d611afc1b1b2f9f
Savy369 <71240740+Savy369@users.noreply.github.com>
2022-01-11 13:13:59 -0800

    fixed bug on courses page; now responsive

--- index.js
+++ index.js
@@ -2,34 +2,34 @@ let courses = [];
 
 //Courses page 
 const addCourseBtn = document.getElementById("addCourseBtn");
-addCourseBtn.addEventListener("click", function() {
+function testFunction() {
     console.log("button clicked");
-    document.getElementById("h1").innerHTML = "test";
-    //(1) makes modal appear (change display to visible) to input course info
+    console.log(document.getElementById("add-course-form"));
     document.getElementById("add-course-form").style.display = "block";
 	//TODO: (4) getCourseNameAndTeacher() function (to code later) that takes a course URL
-	//  and sets courseName and courseTeacher variables to their respective values
-})
+	//  and sets courseName and courseTeacher variables to their respective values 
+}
 
 const submitCourseBtn = document.getElementById("submit-new-course-btn");
 submitCourseBtn.addEventListener("click", function() {
-    //(2) takes input bar value and adds it to Courses[] field in user object
-    let inputVal = document.getElementById("course-url-field").value;
-    //create new course object
+    const urlField = document.getElementById("course-url-field");
+    let inputVal = urlField.value;
+    document.getElementById("add-course-form").style.display = "none";
     let course = {
-        name: "Algorithms and Data Structures",
+        name: inputVal,
         url: inputVal,
         progress: 0,
     }
     courses.push(course);
-    //(3) render() that displays courses
     renderCourses();
+    urlField.value = "";
 })
 
 function renderCourses() {
     const courseList = document.getElementById("course-list");
+    courseList.innerHTML = ""
     for (let i = 0; i < courses.length; i++) {
-        courseList.innerHTML = 
+        courseList.innerHTML += 
             `<li class="box">
                 <img src="../images/course-progress.png" alt="course proress percentage">
                 ${courses[i].name}
