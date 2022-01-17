54001de2ecaaebd6640019509875d8a111991171
Savy369 <71240740+Savy369@users.noreply.github.com>
2022-01-16 20:15:14 -0800

    Updated Courses Page to have dynamic progress bar; still needs some work

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ window.onload = (event) => {
   };
 
 
-let courses = [];
 let testCourse = {         
     name: "Test Course",
     url: "link_url",
     progress: 30,
 }
+let courses = [testCourse]; //TODO: empty
 let currentCourse = testCourse; //TODO: change to null; this is just a placeholder
 
 //Courses page 
@@ -50,12 +50,25 @@ function renderCourses() {
     courseList.innerHTML = "";
     for (let i = 0; i < courses.length; i++) {
         courseList.innerHTML += 
-            `<div class="course1">
+            `<div class="course1" id="course-item">
                 ${courses[i].name}
                 <br>
-                <img src="../images/course-progress.png" alt="course progress percentage" width="170" height="170">
+                <div class="progress-container-courses">
+                    <div id="progress-bar-courses">
+                    </div>
+                </div>
+                Course Progress: ${courses[i].progress}%
             </div>`
-    }
+        document.getElementById("progress-bar-courses").style.width = "30%"; //this is setting that for all courses of this div
+    } //here, make the progress-bar-courses width the correct
+
+    // Add a class in the div using .classList.add()
+    // Target the div of that class of that class and change its width
+
+
+    //Set correct ID's
+    // - for each course in courses.length, give them an id that is course + i
+    // - set the course width of element that has progress-bar-courses to courses[i].progress + %
 }
 
 //Progress Page
